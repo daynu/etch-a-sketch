f2da1f66c649d53635d5b514e9a32066e6546654
daynu <danu111gribincea@gmail.com>
2023-07-21 15:45:47 +0300

    Added black and rainbow modes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 
 window.onload = () =>
-{   
-    let divsContainer = document.getElementById('divs-container')
+{    
 
+    let color = "black";
+    let divsContainer = document.getElementById('divs-container');
     function createDivs(size)
 {   
     divsContainer.innerHTML = '';
@@ -17,21 +18,68 @@ window.onload = () =>
     rows.forEach(
         row => {
             for (let i = 0; i < size; i++)
-            row.innerHTML += "<div class = 'square'><div/>"
+            {
+            let square = document.createElement('div')
+            square.className = 'square'
+            row.append(square)
+            square.addEventListener('mouseover', () =>
+            {
+                
+                if(isRainbow)
+                {
+                    color = randomColor();
+                }
+                square.style.backgroundColor = color;
+            })
+        }  
         }
     )
+    
+    
 }
+
+    
 
     let slider = document.querySelector('.slider');
     let sliderValue = document.getElementById('slide-value')
+    
+    createDivs(slider.value, color)
 
+
+    
     slider.addEventListener("click", () => 
     {
         let value = slider.value;
         sliderValue.innerHTML = slider.value;
-
-
-        createDivs(value)
+        createDivs(value, color);
     })
+
+
+    let black = document.getElementById('black');
+    black.addEventListener('click', ()=>
+    {
+        color = 'black';
+        isRainbow = false;
+    })
+
+    function randomColor()
+    {
+        let r = Math.floor(Math.random() * 256).toString(16);
+        let g = Math.floor(Math.random() * 256).toString(16);
+        let b = Math.floor(Math.random() * 256).toString(16);
+        console.log(r)
+        return '#'+r+g+b
+        
+    }
+    
+
+    let isRainbow = false;
+
+    let rainbow = document.getElementById('rainbow')
+    rainbow.addEventListener('click', ()=>
+    {
+        isRainbow = true;
+    })
+
 
 }
